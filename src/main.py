39d5661f1194760c1c9646e85d0e0a31227f2149
Tulsi Kumar <kumar.1992703@studenti.uniroma1.it>
2025-06-17 10:11:38 +0200

    bug fix

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 api_key = os.getenv("QDRANT_API_KEY")
 qdrant_url = os.getenv("QDRANT_URL")
 # Load config
-config = load_config("config.yaml")
+config = load_config("src/config.yaml")
 
 # Load dataset
 test_cfg = config["test_config"]
@@ -29,7 +29,7 @@ embedding_model = load_hf_embeddings(
 retriever = QdrantRetriever(
     embedding=embedding_model,
     api_key=api_key,  
-    qdrant_url=api_key,
+    qdrant_url=qdrant_url,
     collection_name=config["database"]["collection_name"],
     k=5
 )
