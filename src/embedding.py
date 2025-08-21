305d18997ca3a1a5c310d1448b6185ec6b14d4a6
Tulsi Kumar <kumar.1992703@studenti.uniroma1.it>
2025-08-21 02:36:20 +0200

    qwen different methods integration

--- src/embedding.py
+++ src/embedding.py
@@ -3,11 +3,15 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import logging
+import torch
+from torch import Tensor
+#import vllm
+#from vllm import LLM
 
 # Suppress LangChain warnings
 logging.getLogger("langchain").setLevel(logging.ERROR)
 
-def load_hf_embeddings(model_name: str, normalize: bool = True):
+def load_hf_embeddings(model_name: str, normalize: bool = True, model_type: str):
     """
     Loads a HuggingFaceEmbeddings model with optional normalization.
 
@@ -24,7 +28,14 @@ def load_hf_embeddings(model_name: str, normalize: bool = True):
         return HuggingFaceEmbeddings(model_name=model_name, encode_kwargs=encode_kwargs)
     
     elif model_name=="Qwen/Qwen3-Embedding-4B":
-        return qwen_embedder(model_name=model_name)
+        if model_type=='sentence':
+            return qwen_embedder(model_name=model_name)
+        elif model_type=='vllm':
+            return QwenEmbedderVLLM(model_name=model_name)
+        elif model_type=='transformer':
+            return QwenEmbedder(model_name=model_name)
+        else: 
+            print('model type is not correct supported model types are |sentence|vllm|transformer|')
     
     else:
         print('Embedding model name is incorrect')
@@ -36,17 +47,21 @@ def load_hf_embeddings(model_name: str, normalize: bool = True):
 class qwen_embedder:
     def __init__(self, model_name="Qwen/Qwen3-Embedding-4B"):
         # Load the sentence-transformers model
-        #self.model = SentenceTransformer(model_name)
         self.model = SentenceTransformer(
                                     model_name,
                                     model_kwargs={
                                         "torch_dtype": "auto",       # important: will use float16/bfloat16 automatically
                                         "device_map": "auto",
                                     },
-                                    tokenizer_kwargs={"padding_side": "left"},
-                                )
+                                    tokenizer_kwargs={"padding_side": "left",
+                                                      "max_length": 2048,
+                                                      "truncation": True                                                      
+                                                      }
+                                                      )
 
-    def embed_documents(self, texts, 
+    def embed_documents(self, 
+                        texts,
+                        batch_size=16, 
                         padding=True, 
                         truncation=True, 
                         max_length=2048, 
@@ -66,6 +81,7 @@ class qwen_embedder:
         """
         embeddings = self.model.encode(
             texts,
+            batch_size=batch_size, 
             padding=padding,
             truncation=truncation,
             max_length=max_length,
@@ -76,5 +92,79 @@ class qwen_embedder:
         embeddings = embeddings.tolist()
         return embeddings
 
+
+
+class QwenEmbedderVLLM:
+    def __init__(self, model_name="Qwen/Qwen3-Embedding-4B", batch_size=4):
+        self.model = LLM(model=model_name, task="embed",disable_async_output_proc=True)
+        self.batch_size = batch_size
+
+    def embed_documents(self, texts):
+        """
+        Embeds a list of texts using vLLM in batches and returns a list of lists.
+
+        Args:
+            texts (list[str]): Documents to embed
+
+        Returns:
+            list[list[float]]: Embeddings
+        """
+        all_embeddings = []
+        for i in range(0, len(texts), self.batch_size):
+            batch = texts[i:i + self.batch_size]
+            outputs = self.model.embed(batch)
+            batch_embeddings = [o.outputs.embedding for o in outputs]
+            all_embeddings.extend(batch_embeddings)
+        return all_embeddings
+
+
+
+
+def last_token_pool(last_hidden_states: Tensor,
+                 attention_mask: Tensor) -> Tensor:
+    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+    if left_padding:
+        return last_hidden_states[:, -1]
+    else:
+        sequence_lengths = attention_mask.sum(dim=1) - 1
+        batch_size = last_hidden_states.shape[0]
+        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+
+
+class QwenEmbedder:
+    def __init__(self, model_name="Qwen/Qwen3-Embedding-4B", max_length=2048):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+        model = AutoModel.from_pretrained(
+            model_name,
+            torch_dtype=torch.bfloat16,
+           # attn_implementation="flash_attention_2",
+            device_map='auto',
+        )
+        
+        self.model=model
+        self.max_length = max_length
+
+    def embed_documents(self, texts, batch_size=16, normalize=True):
+        all_embeddings = []
+
+        for i in range(0, len(texts), batch_size):
+            batch = texts[i:i + batch_size]
+            batch_dict = self.tokenizer(
+                batch,
+                padding=True,
+                truncation=True,
+                max_length=self.max_length,
+                return_tensors="pt",
+            ).to(self.model.device)  # send input to CUDA
+
+            with torch.no_grad():
+                outputs = self.model(**batch_dict)
+                embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+                if normalize:
+                    embeddings = F.normalize(embeddings, p=2, dim=1)
+
+            all_embeddings.extend(embeddings.cpu().tolist())
+
+        return all_embeddings
 # Example usage:
 # model = load_hf_embeddings("nasa-impact/nasa-smd-ibm-st-v2")
